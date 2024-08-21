588dff223209e6627c5470a4a9afc85ef05d5dd5
sumansauravmay <sumansauravmay12345@gmail.com>
2024-08-21 12:24:24 +0530

    register

--- jobapp/src/pages/Register.jsx
+++ jobapp/src/pages/Register.jsx
@@ -1,34 +1,56 @@
-'use client'
-
+import axios from "axios";
 import {
   Container,
   Flex,
   Box,
   Heading,
   Text,
-  IconButton,
   Button,
   VStack,
-  HStack,
   Wrap,
   WrapItem,
   FormControl,
   FormLabel,
   Input,
   InputGroup,
-  InputLeftElement,
-  Textarea,
-} from '@chakra-ui/react'
-import {
-  MdPhone,
-  MdEmail,
-  MdLocationOn,
-  MdFacebook,
-  MdOutlineEmail,
-} from 'react-icons/md'
-import { BsGithub, BsDiscord, BsPerson } from 'react-icons/bs'
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const userData = {
+    email,
+    name,
+    password,
+  };
+
+  const handleRegister = () => {
+    if (
+      email.includes("@alphaware.com") ||
+      email.includes("@alphawarenext.com")
+    ) {
+      axios
+        .post(
+          "https://alphaware-io-suman-saurav.onrender.com/useradmin/signup",
+          userData
+        )
+        .then((res) => {
+          console.log(res.data.user);
+          navigate("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      alert("wrond email!");
+    }
+  };
+
   return (
     <Container bg="#9DC4FB" maxW="full" mt={0} centerContent overflow="hidden">
       <Flex>
@@ -37,79 +59,16 @@ export default function Register() {
           color="white"
           borderRadius="lg"
           m={{ sm: 4, md: 16, lg: 10 }}
-          p={{ sm: 5, md: 5, lg: 16 }}>
+          p={{ sm: 5, md: 5, lg: 16 }}
+        >
           <Box p={4}>
             <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
               <WrapItem>
                 <Box>
-                  <Heading>Contact</Heading>
+                  <Heading>Signup Page</Heading>
                   <Text mt={{ sm: 3, md: 3, lg: 5 }} color="gray.500">
-                    Fill up the form below to contact
+                    Fill up the details to Signup
                   </Text>
-                  <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
-                    <VStack pl={0} spacing={3} alignItems="flex-start">
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="200px"
-                        variant="ghost"
-                        color="#DCE2FF"
-                        _hover={{ border: '2px solid #1C6FEB' }}
-                        leftIcon={<MdPhone color="#1970F1" size="20px" />}>
-                        +91-988888888
-                      </Button>
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="200px"
-                        variant="ghost"
-                        color="#DCE2FF"
-                        _hover={{ border: '2px solid #1C6FEB' }}
-                        leftIcon={<MdEmail color="#1970F1" size="20px" />}>
-                        hello@abc.com
-                      </Button>
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="200px"
-                        variant="ghost"
-                        color="#DCE2FF"
-                        _hover={{ border: '2px solid #1C6FEB' }}
-                        leftIcon={<MdLocationOn color="#1970F1" size="20px" />}>
-                        Karnavati, India
-                      </Button>
-                    </VStack>
-                  </Box>
-                  <HStack
-                    mt={{ lg: 10, md: 10 }}
-                    spacing={5}
-                    px={5}
-                    alignItems="flex-start">
-                    <IconButton
-                      aria-label="facebook"
-                      variant="ghost"
-                      size="lg"
-                      isRound={true}
-                      _hover={{ bg: '#0D74FF' }}
-                      icon={<MdFacebook size="28px" />}
-                    />
-                    <IconButton
-                      aria-label="github"
-                      variant="ghost"
-                      size="lg"
-                      isRound={true}
-                      _hover={{ bg: '#0D74FF' }}
-                      icon={<BsGithub size="28px" />}
-                    />
-                    <IconButton
-                      aria-label="discord"
-                      variant="ghost"
-                      size="lg"
-                      isRound={true}
-                      _hover={{ bg: '#0D74FF' }}
-                      icon={<BsDiscord size="28px" />}
-                    />
-                  </HStack>
                 </Box>
               </WrapItem>
               <WrapItem>
@@ -119,34 +78,48 @@ export default function Register() {
                       <FormControl id="name">
                         <FormLabel>Your Name</FormLabel>
                         <InputGroup borderColor="#E0E1E7">
-                          <InputLeftElement pointerEvents="none">
-                            <BsPerson color="gray.800" />
-                          </InputLeftElement>
-                          <Input type="text" size="md" />
+                          <Input
+                            type="text"
+                            size="md"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                          />
                         </InputGroup>
                       </FormControl>
-                      <FormControl id="name">
+
+                      <FormControl id="mail">
                         <FormLabel>Mail</FormLabel>
                         <InputGroup borderColor="#E0E1E7">
-                          <InputLeftElement pointerEvents="none">
-                            <MdOutlineEmail color="gray.800" />
-                          </InputLeftElement>
-                          <Input type="text" size="md" />
+                          <Input
+                            type="text"
+                            size="md"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
                         </InputGroup>
                       </FormControl>
-                      <FormControl id="name">
-                        <FormLabel>Message</FormLabel>
-                        <Textarea
-                          borderColor="gray.300"
-                          _hover={{
-                            borderRadius: 'gray.300',
-                          }}
-                          placeholder="message"
-                        />
+
+                      <FormControl id="password">
+                        <FormLabel>Password</FormLabel>
+                        <InputGroup borderColor="#E0E1E7">
+                          <Input
+                            type="text"
+                            size="md"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                          />
+                        </InputGroup>
                       </FormControl>
-                      <FormControl id="name" float="right">
-                        <Button variant="solid" bg="#0D74FF" color="white" _hover={{}}>
-                          Send Message
+
+                      <FormControl id="" float="right">
+                        <Button
+                          variant="solid"
+                          bg="#0D74FF"
+                          color="white"
+                          _hover={{}}
+                          onClick={handleRegister}
+                        >
+                          Sign UP
                         </Button>
                       </FormControl>
                     </VStack>
@@ -158,5 +131,5 @@ export default function Register() {
         </Box>
       </Flex>
     </Container>
-  )
+  );
 }
